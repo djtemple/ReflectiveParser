73347377708dc64c749c3d3c0ce90901ce998955
rarmclea <rarmclea@ucalgary.ca>
2017-02-27 13:41:54 -0700

    jar stuff & condensed code
    condensed the command line checking, started looking at grabbing the jar file from the command line, condensed code with references to FatalErrors class

--- Interpreter.java
+++ Interpreter.java
@@ -1,73 +1,78 @@
+package parser;
+
+import java.io.IOException;
 import java.lang.reflect.*;
 import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.jar.JarFile;
 
 public class Interpreter {
 	static boolean verbose;
 	static boolean running = true;
-	static Class subject;
+	static String jarname;
+	static String classname = "Commands";
+	static Class<?> subject;
+	static String[] allowables = {"int", "float", "java.lang.String", "java.lang.Float", "java.lang.Integer"};
 	
 	public static void main(String[] args){
 		checkCommandLineArgs(args);
 	}
-	
 	private static void checkCommandLineArgs(String[] args){
 		boolean h = false;
-		String classname = null;
-		String filename = null;
-		for (String arg : args){
-			if (arg.charAt(0) == '-'){
-				if (arg.equals("-v") || arg.equals("--verbose"))
+		int i;
+		for (i = 0; i < args.length; i++){
+			if (args[i].charAt(0) == '-'){
+				if (args[i].equals("-v") || args[i].equals("--verbose"))
 					verbose = true;
-				else if ((arg.equals("-?") || arg.equals("-h")) || arg.equals("--help"))
+				else if ((args[i].equals("-?") || args[i].equals("-h")) || args[i].equals("--help"))
 					h = true;
-				else if (arg.charAt(1) == '-'){
-					System.err.println("Unrecognized qualifier " + arg);
-					synopsis();
-					System.exit(-1);
+				else if (args[i].charAt(1) == '-'){
+					FatalErrors.unrecognizedQualifier(args[i]);
 				}
 				else {
-					System.err.println("Unrecognized qualifier '" + arg.charAt(1) + "' in '" + arg + "'");
-					synopsis();
-					System.exit(-1);
+					FatalErrors.unrecognizedLongQualifier(args[i]);
 				}
 			}
-			else if (filename == null){
-				filename = arg;
-			}
-			else if (classname == null){
-				classname = arg;
-			}
-			else {
-				System.err.println("This program takes at most two command-line arguments");
-				synopsis();
-				System.exit(-2);
-			}
+			else
+				break;
+		}
+		for (int j = 0; i < args.length; i++, j++){
+			if (args[i].charAt(0) == '-')
+				FatalErrors.invalidArgOrder();
+			else if (j == 0)
+				jarname = args[i];
+			else if (j == 1)
+				classname = args[i];
+			else
+				FatalErrors.invalidNumberOfArgs();
 		}
 		if (args.length == 0)
 			synopsis();
-		else if (h && (filename == null)){
+		else if (h && (jarname != null))
+			FatalErrors.invalidHelpQualifier();
+		else if (h){
 			synopsis();
 			System.out.println("\nThis program interprets commands of the format '(<method> {arg}*)' on the command line, finds corresponding");
 			System.out.println("methods in <class-name>, and executes them, printing the result to sysout.");
 		}
-		else if (h && (filename != null)){
-			System.err.println("Qualifier '--help' (-h, -?) should not appear with any command-line arguments");
-			synopsis();
-			System.exit(-4);			
-		}
 		else {
 			try {
+				initJarFile();
 				subject = Class.forName(classname);
+				console();
 			} catch (ClassNotFoundException e) {
 				System.err.println("Could not find class: " + classname);
 				System.exit(-6);
+			} catch (IOException e2){
+				System.err.println("Could not find jar file: " + jarname);
+				System.exit(-5);
 			}
-			console();
 		}
+		
 	}
 	
-	private static void synopsis(){
+	static void synopsis(){
 		System.out.println("Synopsis:");
 		System.out.println("  methods");
 		System.out.println("  methods { -h | -? | --help }+");
@@ -79,7 +84,6 @@ public class Interpreter {
 		System.out.println("  -v --verbose: Print out detailed errors, warning, and tracking.");
 		System.out.println("  -h -? --help: Print out a detailed help message.");
 		System.out.println("Single-char qualifiers may be grouped; long qualifiers may be truncated to unique prefixes and are not case sensitive.");
-
 	}
 	
 	private static void console(){
@@ -130,10 +134,13 @@ public class Interpreter {
 	private static void valueHandler(String input){
 		try {
 			if (input.charAt(0) == '"'){
-				if (input.charAt(input.length()-1) == '"')
-					System.out.println(input);
-				else
+				int close = input.indexOf('"', 1);
+				if (close == -1)
 					throw new ParseException("Reached end of string while parsing", input.length());
+				else if (close < (input.length() - 1))
+					throw new ParseException("Unexpected character encountered at offset " + (close + 1), (close + 1) );
+				else
+					System.out.println(input);
 			}
 			else {
 				int point = 0;
@@ -143,8 +150,7 @@ public class Interpreter {
 					else if (input.charAt(i) == '.')
 						point++;
 					else
-						throw new ParseException("Unexpected character encountered at offset " + i, i);
-					
+						throw new ParseException("Unexpected character encountered at offset " + i, i);					
 				}
 				if (point > 1){
 					int offset = input.indexOf('.', (input.indexOf('.')+1));
@@ -162,29 +168,57 @@ public class Interpreter {
 			if (verbose)
 				e.printStackTrace(); 
 		}
-		
 	}
 	
 	private static void functionHandler(String input){
 		System.out.println("Handling " + input + "...");
 	}
-	
-	
-	private static void listFunctions(Class c){
+
+	private static void listFunctions(Class<?> c){
+		String name;
+		String rType;
 		Method[] methods = c.getMethods();
+		boolean ignore;
 		for (int i = 0; i < methods.length; i++){
-			System.out.print("(");
-			System.out.print(methods[i].getName());
+			ignore = false;
+			name = methods[i].getName();
+			rType = methods[i].getReturnType().getName();
+			if (Arrays.asList(allowables).contains(rType) == false)
+				continue;
 			Parameter[] parameters = methods[i].getParameters();
 			for (int j = 0; j < parameters.length; j++){
-				System.out.print(" " + parameters[j].getType().getName());
+				if (Arrays.asList(allowables).contains(parameters[j].getType().getName()) == false)
+					ignore = true;
 			}
-			System.out.print(") : ");
-			System.out.println(methods[i].getReturnType().getName());
+			if (ignore)
+				continue;
+			System.out.print("(" + name);
+			for (int j = 0; j < parameters.length; j++){
+				if (parameters[j].getType().getName().equals("java.lang.String"))
+					System.out.print(" String");
+				else if (parameters[j].getType().getName().equals("java.lang.Integer"))
+					System.out.print(" int");
+				else if (parameters[j].getType().getName().equals("java.lang.Float"))
+					System.out.print(" float");
+				else
+					System.out.print(" " + parameters[j].getType().getName());
+			}
+
+			if (rType.equals("java.lang.String"))
+				System.out.println(") : String");
+			else if (rType.equals("java.lang.Integer"))
+				System.out.println(") : int");
+			else if (rType.equals("java.lang.Float"))
+				System.out.println(") : float");
+			else
+				System.out.println(") : " + rType);
 			
 		}
 	}
-	
+	private static void initJarFile() throws IOException{
+		JarFile j = new JarFile(jarname);
+		j.close();
+	}
 	private static void help(){
 		System.out.println("q           : Quit the program.");
 		System.out.println("v           : Toggle verbose mode (stack traces).");
